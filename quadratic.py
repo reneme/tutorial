a2a51ebe85a50eec72c214ebb1a5db16bb33cec5
Rene Meusel <rene@renemeusel.de>
2014-09-29 19:36:26 +0200

    add quadratic.py

---
+++ quadratic.py
@@ -0,0 +1,11 @@
+import sys
+
+def usage():
+    print "This calculates the square root of a given number"
+    print sys.argv[0] , "<number>"
+
+if len(sys.argv) != 2:
+    usage()
+    sys.exit(1)
+
+print "Implement me! I feel so empty :("
